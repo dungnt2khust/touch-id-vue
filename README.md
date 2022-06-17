2b76af149322e0f1aa9abc03955792d1d2e93525
dungnt2khust <dunggamo2000@gmail.com>
2022-06-17 10:27:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-6ebby8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-6ebby8)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TouchID Side="Right" :Percent="48" />
+  </div>
+</template>
+
+<script>
+import TouchID from './components/TouchID.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TouchID,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* #chat-wi */
+</style>
